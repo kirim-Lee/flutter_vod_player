c32946f4b892d0b602b4488485db89694eb11682
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2023-05-06 16:25:36 +0900

    empty rendering

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,84 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}): super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('home Screen'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderEmpty () {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          _Logo(),
+          SizedBox(height:30.0),
+          _AppName(),
+        ],
+      ),
+    );
+  }
+
+  Widget renderVideo() {
+    return Container();
+  }
+
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}): super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('asset/img/logo.png');
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}): super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('VIDEO', style: textStyle),
+        Text('PLAYER', style: textStyle.copyWith(fontWeight: FontWeight.w700),),
+      ],
     );
   }
 }
 
+BoxDecoration getBoxDecoration() {
+  return const BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xFF2A3A7C),
+        Color(0xFF000118),
+      ]
+    )
+  );
+}
